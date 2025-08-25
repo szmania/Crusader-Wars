0c8f0bdf527618c541ee1311d46ca1ce833af1a8
Your Name <szmania@yahoo.com>
2025-08-25 12:13:38 -0700

    feat: Add unit creation and organization logic for armies.

--- CrusaderWars/data/save_file/Armies_Functions.cs
+++ CrusaderWars/data/save_file/Armies_Functions.cs
@@ -278,23 +278,26 @@ namespace CrusaderWars.data.save_file
                     {
                         if (knight.GetCultureObj() == null)
                         {
-                            Program.Logger.Debug($"Knight {knight.GetID()} in army {army.ID} has null culture. Assigning fallback culture.");
                             Culture mainParticipantCulture = null;
                             if (army.IsPlayer())
                             {
                                 string id = CK3LogData.LeftSide.GetMainParticipant().culture_id;
+                                Program.Logger.Debug($"Knight {knight.GetID()} in army {army.ID} has null culture. Assigning fallback culture. " +
+                                                     $"Assigning fallback culture ID: {id}");
                                 mainParticipantCulture = new Culture(id);
                             }
                             else if (army.IsEnemy())
                             {
                                 string id = CK3LogData.RightSide.GetMainParticipant().culture_id;
+                                Program.Logger.Debug($"Knight {knight.GetID()} in army {army.ID} has null culture. Assigning fallback culture. " +
+                                                     $"Assigning fallback culture ID: {id}");
                                 mainParticipantCulture = new Culture(id);
                             }
 
                             Culture new_culture = army.Knights.GetKnightsList()?.Find(x => x.GetCultureObj() != null)?.GetCultureObj() ?? mainParticipantCulture;
                             
                             string newCultureID = new_culture?.ID ?? "no_culture_found";
-                            Program.Logger.Debug($"Knight {knight.GetID()} in army {army.ID} has null culture. " +
+                            Program.Logger.Debug($"Knight {knight.GetID()} in army {army.ID} has null culture. Assigning fallback culture. " +
                                 $"Assigning fallback culture ID: {newCultureID}");
                             
                             knight.ChangeCulture(new_culture);
@@ -536,5 +539,203 @@ namespace CrusaderWars.data.save_file
          *##############################################
          */
 
+        internal static List<Unit> GetAllUnits_UnitKeys(List<Unit> units)
+        {
+            Program.Logger.Debug("Getting Attila unit keys for all units...");
+            //Set Unit Keys
+            foreach (var unit in units)
+            {
+                if (unit.GetRegimentType() == RegimentType.Levy) continue;
+
+                string key = UnitMappers_BETA.GetUnitKey(unit);
+                if (key == "not_found")
+                {
+                    Program.Logger.Debug($"Unit key not found for '{unit.GetName()}' ({unit.GetCulture()}). Using default 'cha_spa_royal_cav'.");
+                    unit.SetUnitKey("cha_spa_royal_cav");
+                }
+                else
+                    unit.SetUnitKey(key);
+            }
+            Program.Logger.Debug("Finished getting unit keys.");
+            return units;
+        }
+
+        internal static List<Unit> GetAllUnits_AttilaFaction(List<Unit> units)
+        {
+            Program.Logger.Debug("Getting Attila factions for all units...");
+            //Get Unit Mapper Faction
+            foreach (var unit in units)
+            {
+                unit.SetAttilaFaction(UnitMappers_BETA.GetAttilaFaction(unit.GetCulture(), unit.GetHeritage()));
+            }
+            Program.Logger.Debug("Finished getting Attila factions.");
+            return units;
+        }
+        internal static List<Unit> GetAllUnits_Max(List<Unit> units)
+        {
+            Program.Logger.Debug("Getting max unit sizes for all units...");
+            //Read Unit Limit
+            foreach (var unit in units)
+            {
+                unit.SetMax(UnitMappers_BETA.GetMax(unit));
+            }
+            Program.Logger.Debug("Finished getting max unit sizes.");
+            return units;
+        }
+
+        // Function to get the top N units by soldiers count
+        internal static List<Unit> GetTopUnits(List<Unit> units, int topN)
+        {
+            Program.Logger.Debug($"Getting top {topN} units by soldier count.");
+            if (topN <= 0)
+            {
+                throw new ArgumentException("topN must be greater than 0");
+            }
+
+            // Sort units by soldiers count in descending order
+            var sortedUnits = units.OrderByDescending(u => u.GetSoldiers()).ToList();
+
+            // Take the top N units
+            return sortedUnits.Take(topN).ToList();
+        }
+
+        internal static void CreateUnits(List<Army> armies)
+        {
+            Program.Logger.Debug("Creating units from regiments for all armies...");
+            foreach (var army in armies)
+            {
+                Program.Logger.Debug($"Creating units from {armies.Count} total armies, for army {army.ID}");
+                List<(Regiment regiment, RegimentType type, string maa_name)> list = new List<(Regiment regiment, RegimentType type, string maa_name)>();
+                foreach (var army_regiment in army.ArmyRegiments)
+                {
+                    foreach (var regiment in army_regiment.Regiments)
+                    {
+                        list.Add((regiment, army_regiment.Type, army_regiment.MAA_Name));
+                    }
+                }
+
+                List<Unit> units = new List<Unit>();
+                foreach (var regiment in list)
+                {
+                    // if no soldiers, skip
+                    if (ModOptions.FullArmies(regiment.regiment) is null) continue;
+
+                    if (Int32.Parse(ModOptions.FullArmies(regiment.regiment)) == 0) continue;
+
+                    Unit unit;
+                    if (regiment.type == RegimentType.Levy)
+                    {
+                        Culture levyCulture = regiment.regiment.Culture ?? army.Owner.GetCulture();
+                        if (regiment.regiment.isMercenary())
+                            unit = new Unit("Levy", Int32.Parse(ModOptions.FullArmies(regiment.regiment)), levyCulture, regiment.type, true);
+                        else
+                            unit = new Unit("Levy", Int32.Parse(ModOptions.FullArmies(regiment.regiment)), levyCulture, regiment.type);
+                    }
+                    else if (regiment.type == RegimentType.MenAtArms)
+                        if (regiment.regiment.isMercenary())
+                            unit = new Unit(regiment.maa_name, Int32.Parse(ModOptions.FullArmies(regiment.regiment)), regiment.regiment.Culture, regiment.type, true);
+                        else
+                            unit = new Unit(regiment.maa_name, Int32.Parse(ModOptions.FullArmies(regiment.regiment)), regiment.regiment.Culture, regiment.type);
+                    else
+                        continue;
+
+                    if (unit != null)
+                        units.Add(unit);
+
+
+                }
+
+                units = OrganizeUnitsIntoCultures(units, army.Owner);
+                units = OrganizeLeviesUnits(units);
+                units = GetAllUnits_AttilaFaction(units);
+                units = GetAllUnits_Max(units);
+                units = GetAllUnits_UnitKeys(units);
+                army.SetUnits(units);
+                Program.Logger.Debug($"Finished creating {units.Count} units for army {army.ID}.");
+                //army.PrintUnits();
+            }
+            Program.Logger.Debug("Finished creating units for all armies.");
+        }
+
+        static List<Unit> OrganizeUnitsIntoCultures(List<Unit> units, Owner owner)
+        {
+            Program.Logger.Debug($"Organizing {units.Count} units by culture for owner {owner.GetID()}");
+            foreach (var unit in units)
+            {
+                Program.Logger.Debug($"- Unit: {unit.GetName()}, Soldiers: {unit.GetSoldiers()}, " +
+                    $"Culture: {unit.GetObjCulture()?.GetCultureName() ?? "null"}, " +
+                    $"Heritage: {unit.GetObjCulture()?.GetHeritageName() ?? "null"}");
+            }
+
+            var organizedUnits = new List<Unit>();
+
+            // Group units by Name and Culture
+            var groupedUnits = units.GroupBy(u => new { Name = u.GetName(), Culture = u.GetCulture(), Type = u.GetRegimentType(), IsMerc = u.IsMerc() });
+
+            // Merge units with the same Name and Culture
+            foreach (var group in groupedUnits)
+            {
+                int totalSoldiers = group.Sum(u => u.GetSoldiers());
+
+                // Create a new Unit with the merged NumberOfSoldiers
+                Unit mergedUnit = new Unit(group.Key.Name, totalSoldiers, group.ElementAt(0).GetObjCulture(), group.ElementAt(0).GetRegimentType(), group.ElementAt(0).IsMerc(), owner);
+                
+                organizedUnits.Add(mergedUnit);
+            }
+            Program.Logger.Debug($"Organized {units.Count} units into {organizedUnits.Count} merged units.");
+            return organizedUnits;
+        }
+
+        static List<Unit> OrganizeLeviesUnits(List<Unit> units)
+        {
+            var unitsBelowThreshold = units.Where(u => u.GetSoldiers() <= ModOptions.CulturalPreciseness() && u.GetName() == "Levy").ToList();
+            Program.Logger.Debug($"Organizing {unitsBelowThreshold.Count} levy units below threshold {ModOptions.CulturalPreciseness()}");
+            if (unitsBelowThreshold.Count == 0) return units;
+
+            int total = 0;
+            Unit biggest = null;
+            int lastRegistered = 0;
+            foreach (var u in unitsBelowThreshold)
+            {
+                total += u.GetSoldiers();
+
+                if (u.GetSoldiers() > lastRegistered)
+                {
+                    lastRegistered = u.GetSoldiers();
+                    biggest = u;
+                }
+
+            }
+
+            var unit = new Unit("Levy", total, biggest.GetObjCulture(), RegimentType.Levy);
+            var unit_data = UnitsFile.RetriveCalculatedUnits(unit.GetSoldiers(), ModOptions.GetLevyMax());
+            var levies_top_cultures = GetTopUnits(unitsBelowThreshold, unit_data.UnitNum);
+
+            int null_soldiers = 0;
+
+            var null_cultures_levies = units.Where(x => x.GetObjCulture() == null).ToList();
+            Console.WriteLine($"Found {null_cultures_levies.Count} null-culture levies");
+            if (null_cultures_levies.Count > 0)
+            {
+                foreach (var null_levie in null_cultures_levies)
+                {
+                    null_soldiers += null_levie.GetSoldiers();
+                    units.Remove(null_levie);
+                }
+            }
+
+
+            int limit = ModOptions.CulturalPreciseness();
+            units.RemoveAll(x => x.GetName() == "Levy" && x.GetSoldiers() <= limit);
+            for (int i = 0; i < unit_data.UnitNum; i++)
+            {
+                if (i == 0)
+                    units.Add(new Unit("Levy", unit_data.UnitSoldiers + null_soldiers, levies_top_cultures[i].GetObjCulture(), RegimentType.Levy));
+                else
+                    units.Add(new Unit("Levy", unit_data.UnitSoldiers, levies_top_cultures[i].GetObjCulture(), RegimentType.Levy));
+            }
+
+            return units;
+        }
     }
 }
