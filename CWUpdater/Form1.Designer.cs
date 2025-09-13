6deea316f0a48a41a7729b8acb57150757b36632
Your Name <szmania@yahoo.com>
2025-09-13 13:36:22 -0700

    feat: Add version label to the form

--- CWUpdater/Form1.Designer.cs
+++ CWUpdater/Form1.Designer.cs
@@ -38,6 +38,17 @@
             this.VersionLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
+            // VersionLabel
+            // 
+            this.VersionLabel.BackColor = System.Drawing.Color.Transparent;
+            this.VersionLabel.Font = new System.Drawing.Font("Paradox King Script", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.VersionLabel.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.VersionLabel.Location = new System.Drawing.Point(0, 45);
+            this.VersionLabel.Name = "VersionLabel";
+            this.VersionLabel.Size = new System.Drawing.Size(750, 25);
+            this.VersionLabel.TabIndex = 8;
+            this.VersionLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // btnUpdate
             // 
             this.btnUpdate.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
