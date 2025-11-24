845842857e1086b009593cbee4a1b6a2ed99f1c7
Your Name <szmania@yahoo.com>
2025-11-24 12:13:04 -0800

    fix: Add missing `using` statement in UnitsCardsNames.cs

--- CrusaderWars/locs/UnitsCardsNames.cs
+++ CrusaderWars/locs/UnitsCardsNames.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Xml.Linq;
+using CrusaderWars.client;
 using CrusaderWars.data.save_file;
 using CrusaderWars.unit_mapper;
 
