1b750373d18bcd6507a8cbd5ccb3960053a1acbe
Your Name <szmania@yahoo.com>
2025-12-07 00:23:59 -0800

    feat: Introduce BattleDetails static class to store battle name

---
+++ CrusaderWars/twbattle/BattleDetails.cs
@@ -0,0 +1,12 @@
+namespace CrusaderWars.twbattle
+{
+    public static class BattleDetails
+    {
+        public static string? BattleName { get; private set; }
+
+        public static void SetBattleName(string name)
+        {
+            BattleName = name;
+        }
+    }
+}
