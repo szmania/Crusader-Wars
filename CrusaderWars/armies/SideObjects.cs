ffa89f91d98be8d699c1ee1ebb9965550076ff35
Your Name <szmania@yahoo.com>
2025-12-07 21:14:19 -0800

    feat: Add methods to calculate total soldiers and losses

--- CrusaderWars/armies/SideObjects.cs
+++ CrusaderWars/armies/SideObjects.cs
@@ -122,6 +122,24 @@ namespace CrusaderWars
             }
         }
 
+        public int GetTotalSoldiers()
+        {
+            if (ArmyRegiments == null) return 0;
+            return ArmyRegiments.Sum(x => x.CurrentNum);
+        }
+
+        public int GetTotalLosses()
+        {
+            if (ArmyRegiments == null) return 0;
+            return ArmyRegiments.Sum(ar => ar.GetLosses());
+        }
+
+        public int GetTotalSoldiersAfterBattle()
+        {
+            if (ArmyRegiments == null) return 0;
+            return ArmyRegiments.Sum(ar => ar.CurrentNum);
+        }
+        
         public void ScaleUnits(int ratio)
         {
             if (ratio > 0)
