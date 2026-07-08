3a69558bc855eb81f1d72760ab260e53292f01b7
Your Name <you@example.com>
2026-07-07 20:42:27 -0700

    fix: Revert unit key matching in UnitsCardsNames.cs

--- CrusaderWars/locs/UnitsCardsNames.cs
+++ CrusaderWars/locs/UnitsCardsNames.cs
@@ -167,7 +167,7 @@ namespace CrusaderWars.locs
                             string currentAttilaKey = keyMatch.Groups[1].Value;
                             var matchingUnitsFromAllArmies = allArmies
                                 .SelectMany(a => a.Units.Select(u => new { Unit = u, Army = a }))
-                                .Where(x => x.Unit.OriginalAttilaKey == currentAttilaKey)
+                                .Where(x => x.Unit.GetAttilaUnitKey() == currentAttilaKey)
                                 .ToList();
 
                             if (matchingUnitsFromAllArmies.Any())
