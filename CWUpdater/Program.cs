10c1ae783f76bef8262214dfc40f130beb4f5908
Your Name <you@example.com>
2026-07-29 16:42:52 -0700

    fix: Hide console window on application startup

--- CWUpdater/Program.cs
+++ CWUpdater/Program.cs
@@ -3,17 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace CWUpdater
 {
     internal static class Program
     {
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        const int SW_HIDE = 0;
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            ShowWindow(GetConsoleWindow(), SW_HIDE);
             Logger.Log("CWUpdater started.");
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
