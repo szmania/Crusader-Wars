9e263c4ea8a374136463ad35803b41f332a4e6a0
Your Name <you@example.com>
2026-07-29 20:41:30 -0700

    fix: Allow infoLabel to wrap text and increase width

--- CrusaderWars/client/MainFile.Designer.cs
+++ CrusaderWars/client/MainFile.Designer.cs
@@ -96,7 +96,8 @@
             // infoLabel
             // 
             resources.ApplyResources(this.infoLabel, "infoLabel");
-this.infoLabel.AutoSize = false;
+this.infoLabel.AutoSize = true;
+this.infoLabel.MaximumSize = new System.Drawing.Size(600, 0);
 this.infoLabel.ForeColor = System.Drawing.Color.White;
             // 
             // SettingsBtn
